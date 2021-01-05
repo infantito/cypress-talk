f4ee143d11987a5c76e6023cfc582983f73e5503
infantito <danielinfantito61@gmail.com>
2021-01-04 23:22:20 -0500

    test: ✅ test the typing of the login credentials

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -2,5 +2,11 @@
 describe('login', () => {
   it('should login an existing user', () => {
     cy.visit('/')
+
+    // Get an input, type into it and verify that the value has been updated
+
+    cy.get('#username').type('fake_user@email.com').should('have.value', 'fake_user@email.com')
+
+    cy.get('#password').type('123456').should('have.value', '123456')
   })
 })
