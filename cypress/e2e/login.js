6ced574a27705e0cd9ef1b55e573cb71d36bc4cc
infantito <danielinfantito61@gmail.com>
2021-01-04 23:57:12 -0500

    test: ✅ test call api

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -3,10 +3,28 @@ describe('login', () => {
   it('should login an existing user', () => {
     cy.visit('/')
 
-    // Get an input, type into it and verify that the value has been updated
+    const username = 'Bret'
 
-    cy.get('#username').type('fake_user@email.com').should('have.value', 'fake_user@email.com')
+    const password = '123456'
 
-    cy.get('#password').type('123456').should('have.value', '123456')
+    // -- Get an input, type into it and verify that the value has been updated
+
+    cy.get('#username').type(username).should('have.value', username)
+
+    cy.get('#password').type(password).should('have.value', password)
+
+    // -- Sign in user
+
+    // Listen to GET to /users
+
+    cy.intercept('GET', '**/users').as('login')
+
+    cy.get('button').click()
+
+    cy.wait('@login').should(({ response }) => {
+      expect(response.statusCode).to.eq(200)
+
+      expect(response.body).to.be.an('array')
+    })
   })
 })
