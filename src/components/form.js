9f21a83cad6186ecffd7f955f7dd3c9124ee451b
infantito <danielinfantito61@gmail.com>
2021-01-04 22:50:04 -0500

    feat: ✨ form

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,44 @@
+import * as React from 'react'
+import '../styles/form.css'
+
 const Form = () => {
+  const [credentials, setCredentials] = React.useState({
+    username: '',
+    password: '',
+  })
+
+  const handleChange = event => {
+    const { target } = event
+
+    setCredentials(state => ({
+      ...state,
+      [target.id]: target.value,
+    }))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    alert(JSON.stringify(credentials, null, 2))
+  }
+
   return (
-    <div className="app">
-      <header className="app-header">
-        <p>
-          Edit <code>src/components/form.js</code> and save to reload.
-        </p>
-        <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form" onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Sign in</legend>
+        <div className="form-block">
+          <label htmlFor="username">username:</label>
+          <input type="text" name="username" id="username" value={credentials.username} onChange={handleChange} />
+        </div>
+        <div className="form-block">
+          <label htmlFor="password">password:</label>
+          <input type="password" name="password" id="password" value={credentials.password} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">Sign in</button>
+        </div>
+      </fieldset>
+    </form>
   )
 }
 
